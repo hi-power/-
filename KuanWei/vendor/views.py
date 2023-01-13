8549cadb4c23860a96f568ce1f44828c73844ffb
Kuang wei <60306hi603@gmail.com>
2023-01-14 07:15:39 +0800

    Day17 templates with app

--- KuanWei/vendor/views.py
+++ KuanWei/vendor/views.py
@@ -5,4 +5,4 @@ from .models import Vendor
 def vendor_index(request):#名稱對應urls.py urlpatterns中的path
     vendor_list = Vendor.objects.all() # 把所有 Vendor 的資料取出來
     context = {'vendor_list': vendor_list} # 建立 Dict對應到Vendor的資料，
-    return render(request, 'test.html',context)
+    return render(request, 'vendors/vendor_detail.html',context)
